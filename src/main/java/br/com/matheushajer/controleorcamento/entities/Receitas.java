b66f2f31c5ff47a3b6a893454cada445bcc468cc
matheushajer <matheushajer@gmail.com>
2022-08-01 19:51:37 -0300

    Entidades receita e despesas criadas

---
+++ src/main/java/br/com/matheushajer/controleorcamento/entities/Receitas.java
@@ -0,0 +1,72 @@
+package br.com.matheushajer.controleorcamento.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_receita")
+public class Receitas implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	private Double valor;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant data;
+
+	public Receitas() {
+	}
+
+	public Receitas(Long id, String descricao, Double valor, Instant data) {
+		this.id = id;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.data = data;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Instant getData() {
+		return data;
+	}
+
+	public void setData(Instant data) {
+		this.data = data;
+	}
+
+}
